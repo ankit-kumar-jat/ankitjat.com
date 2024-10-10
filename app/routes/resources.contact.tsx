50aef6e5492bbec36403a8a5b63b098735aba4e5
Ankit Kumar Jat <ankit.kumar@affle.com>
2024-10-10 19:27:26 +0530

    feat: contact form submission

--- app/routes/resources.contact.tsx
+++ app/routes/resources.contact.tsx
@@ -1,35 +1,94 @@
 import { ActionFunctionArgs, json } from '@remix-run/cloudflare'
-import { Form } from '@remix-run/react'
+import { Form, useFetcher } from '@remix-run/react'
 import { Button } from '~/components/ui/button'
+import { EMAIL } from '~/config'
 import { cn } from '~/lib/utils'
 
-export async function action({ request }: ActionFunctionArgs) {
+export async function action({ request, context }: ActionFunctionArgs) {
   const formData = await request.formData()
 
-  const name = formData.get('name')
-  const email = formData.get('email')
-  const message = formData.get('message')
+  const name = formData.get('name') ?? ''
+  const email = formData.get('email') ?? ''
+  const message = formData.get('message') ?? ''
 
-  // if (redirectTo && typeof redirectTo !== 'string') {
-  //   return json(
-  //     { errorMessage: 'Invalid redirectTo received', success: false },
-  //     { status: 400 },
-  //   )
-  // }
+  // !NOTE: following input is a honeypot
+  const honeypot = formData.get('nameConfirm')
 
-  return json({ success: true })
+  if (honeypot)
+    return json(
+      { success: false, errorMessage: '', errors: {} },
+      { status: 400 },
+    )
+
+  const errors: { name?: string; email?: string; message?: string } = {}
+
+  if (!name || typeof name !== 'string') {
+    errors.name = 'Name is required.'
+  }
+
+  if (!email || typeof email !== 'string') {
+    errors.email = 'Email is required.'
+  }
+
+  if (!message || typeof message !== 'string') {
+    errors.message = 'Message is required.'
+  }
+
+  if (errors.name || errors.email || errors.message) {
+    return json(
+      {
+        errorMessage: 'Please enter all required fields.',
+        success: false,
+        errors,
+      },
+      { status: 400 },
+    )
+  }
+
+  const body = new FormData()
+
+  body.set('name', name)
+  body.set('email', email)
+  body.set('message', message)
+  body.set('formDataNameOrder', `["name","email","message"]`)
+  body.set('formGoogleSheetName', 'responses')
+  body.set('formGoogleSendEmail', EMAIL)
+
+  const URL = context.cloudflare.env.GOOGLE_CONTACT_MACRO_URL
+
+  await fetch(URL, { method: 'post', body })
+
+  return json({ success: true, errors: null })
 }
 
+type FormState = 'idle' | 'success' | 'error' | 'submitting'
+
 export function ContactForm() {
+  const fetcher = useFetcher<typeof action>()
+
+  const state: FormState =
+    fetcher.state === 'submitting'
+      ? 'submitting'
+      : fetcher.data?.success
+        ? 'success'
+        : fetcher.data?.errors
+          ? 'error'
+          : 'idle'
+
   return (
     <div className="grid grid-cols-[1/1]">
       <div
         className={cn(
           'pointer-events-none scale-75 opacity-0 transition-[transform,opacity] delay-0 duration-300 [grid-area:1/1]',
-          true && 'pointer-events-auto scale-100 opacity-100 delay-300',
+          state !== 'success' &&
+            'pointer-events-auto scale-100 opacity-100 delay-300',
         )}
       >
-        <Form action="/" method="POST" className="flex flex-col gap-4">
+        <fetcher.Form
+          method="POST"
+          action="/resources/contact"
+          className="flex flex-col gap-4"
+        >
           <label className="relative">
             <input
               type="text"
@@ -67,16 +126,26 @@ export function ContactForm() {
           {/* NOTE: following input is a honeypot */}
           <input type="text" name="nameConfirm" defaultValue="" hidden />
           <div>
-            <Button type="submit" size="lg">
-              <span>Send Message</span>
+            <Button
+              type="submit"
+              size="lg"
+              disabled={state === 'submitting'}
+              className="min-w-40"
+            >
+              {state === 'submitting' ? (
+                <span>Sending...</span>
+              ) : (
+                <span>Send Message</span>
+              )}
             </Button>
           </div>
-        </Form>
+        </fetcher.Form>
       </div>
       <div
         className={cn(
           'pointer-events-none scale-75 opacity-0 transition-[transform,opacity] delay-0 duration-300 [grid-area:1/1]',
-          false && 'pointer-events-auto scale-100 opacity-100 delay-300',
+          state === 'success' &&
+            'pointer-events-auto scale-100 opacity-100 delay-300',
         )}
       >
         <h3 className="text-2xl drop-shadow-md sm:text-3xl lg:text-4xl">
